923fe67d1887c571f78d2158a571c53df88e3c98
moalmatry <moalmatry2000@gmail.com>
2024-11-13 16:53:14 +0200

    fix: logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,20 +1,18 @@
 import logger from "pino";
-import config from "config";
 import dayjs from "dayjs";
+import config from "config";
+
 const level = config.get<string>("logLevel");
 
 const log = logger({
   transport: {
     target: "pino-pretty",
-    options: {
-      colorize: true,
-    },
   },
   level,
   base: {
     pid: false,
   },
-  timestamp: () => `,"time":${dayjs().format()}`,
+  timestamp: () => `,"time":"${dayjs().format()}"`,
 });
 
 export default log;
