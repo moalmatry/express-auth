64d13b983dd7d3557671cc7f49ab142a0bbf86f6
moalmatry <moalmatry2000@gmail.com>
2024-11-19 06:21:07 +0200

    feat: user controller docs

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -2,6 +2,13 @@ import { NextFunction, Request, Response } from 'express';
 import catchAsync from '../utils/catchAsync';
 import { getUsers } from '../services/user.service';
 
+/** @description returns all users from db
+ *  @example   res.status(200).json({
+    status: 'success',
+    results: users.length,
+    data: { users },
+  });
+ */
 export const getAllUsers = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
   const users = await getUsers();
 
