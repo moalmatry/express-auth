0eb819d37ae9df1cdc68b2bbbfa51f70c9060dc0
moalmatry <moalmatry2000@gmail.com>
2024-11-29 20:26:08 +0200

    feat: add vercel config

--- src/middleware/restrictTo.ts
+++ src/middleware/restrictTo.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { Role } from '@prisma/client';
 import { NextFunction, Response } from 'express';
 import { CustomRequest } from '../types';
@@ -6,6 +7,7 @@ import AppError from '../utils/AppError';
 export const restrictTo =
   (...roles: Role[]) =>
   (req: CustomRequest, res: Response, next: NextFunction) => {
+    // @ts-expect-error
     const restrictedRoles = roles.includes(req.user.role);
     if (!restrictedRoles) {
       return next(new AppError("You don't have permission to perform this action", 403));
